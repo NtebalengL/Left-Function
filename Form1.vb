d72b54f2b8bcfe8d63f5e7da567c2c348392d7e2
Dell-User <Dell-User@DESKTOP-I2HH80B>
2023-10-05 06:37:58 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Microsoft.VisualBasic.Left("Microsoft Visual", 5)-Wrong. Did not declare the data type and prompt the output into a msgbox
+
+        Dim FirstName As String
+        FirstName = Microsoft.VisualBasic.Left("Ntebaleng Letaoana", 9)
+        MsgBox(FirstName)
+
+
+    End Sub
+End Class
